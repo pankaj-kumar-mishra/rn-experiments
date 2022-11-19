14095c46188662b71fb7be3a6179c74961b6fccd
Pankaj Mishra <pankaj_mishra@epam.com>
2022-11-19 07:22:11 +0530

    initial commit

---
+++ App.tsx
@@ -0,0 +1,39 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
+
+// import {PhotoView} from './src/photos-with-swatches';
+// import {ShowFlag} from './src/small-features';
+// import {CustomTable, RNtcTable} from './src/tables';
+import {CardList} from './src/cardlist-tutorial';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    flex: 1,
+    backgroundColor: isDarkMode ? '#000' : '#fff',
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      {/* <CustomTable /> */}
+      {/* <RNtcTable /> */}
+      {/* <ShowFlag /> */}
+      {/* <PhotoView /> */}
+      <CardList />
+    </SafeAreaView>
+  );
+};
+
+export default App;
